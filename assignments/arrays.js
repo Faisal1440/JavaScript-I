0bc4c805786439bd034d9869c37b582941172120
Faisal Aziz <faziz6006@gmail.com>
2019-10-15 15:48:51 -0500

    last sub

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -122,12 +122,16 @@ console.log(cYears(),"carYears");
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-for (let i = 2000; i < inventory.length; i++) {
-  oldCars.push()
+for (let i = 0; i < inventory.length; i++) {
+  oldCars.push(inventory[i].car_Year);
 }
-console.log();
+console.log(oldCars);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
+for (let i = 0; i < inventory.length; i++) {
+if (inventory [i] )
+
+}
 console.log();
